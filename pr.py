86e6b650eb4c262a9d39597f812920d4ddcb03ab
kanikaguptaiitm <kanikagupta.iitm@gmail.com>
2019-10-28 23:56:12 +0530

    Added logging window

--- pr.py
+++ pr.py
@@ -94,6 +94,14 @@ class Page1(Page):
         # geofile=filedialog.askopenfilename(title='Select file', filetypes=(('Geo files', '*.geo'), ('all files', '*.*')))
         # geofilename.set(ntpath.basename(geofile))
         # os.system("cp %s %s/%s/pre-processing"%(geofile, folder, project_name))
+          timeStr = time.asctime()
+          msg="Time: "+timeStr
+          logging.info(msg)
+          filehandle.write(msg+"\n")
+          msg="%s is now present in pre-processing folder."%geofilename.get()
+          logging.info(msg)
+          logging.info("\n")
+          filehandle.write(msg+"\n")
           buttonimp1.config(state="normal")
         openfile()
         bottom.bind('<Control-o>', openfile)
@@ -102,7 +110,23 @@ class Page1(Page):
        button.place(relwidth=0.15, relheight=0.15, rely=0.1, relx=0.8)
        
        def createmsh():
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="Creating %s.msh..."%os.path.splitext(geofilename.get())[0]
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        logging.info("\n")
         # os.system("gmsh -2 %s/%s/pre-processing/%s"%(folder, project_name,geofilename.get()))
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="%s.msh is created and is saved in pre-processing folder."%os.path.splitext(geofilename.get())[0]
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
         buttonimp2.config(state="normal")
         filename=geofilename.get()
         msh=os.path.splitext(filename)[0]
@@ -142,6 +166,14 @@ class Page1(Page):
         # mshfile=filedialog.askopenfilename(title='Select file', filetypes=(('Msh files', '*.msh'), ('all files', '*.*')))
         # mshfilename.set(ntpath.basename(mshfile))
         # os.system("cp %s %s/%s/pre-processing"%(mshfile, folder, project_name))
+          timeStr = time.asctime()
+          msg="Time: "+timeStr
+          logging.info(msg)
+          filehandle.write(msg+"\n")
+          msg="%s is present in pre-processing folder."%mshfilename.get()
+          logging.info(msg)
+          logging.info("\n")
+          filehandle.write(msg+"\n")
           buttonimp2.config(state="normal")
           global sol1
           global sol2
@@ -155,12 +187,19 @@ class Page1(Page):
        button.place(relwidth=0.15, relheight=0.15, rely=0.55, relx=0.8)
 
        def showmsh():
-        print("some")
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="%s is visible."%mshfilename.get()
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
         # os.system("gmsh %s/%s/pre-processing/%s"%(folder, project_name,mshfilename.get()))
        buttonimp2=tk.Button(bottom, text="Show msh", font=helv36, state="disabled", command=showmsh)
        buttonimp2.pack()
        buttonimp2.place(relwidth=0.20, relheight=0.15, rely=0.75, relx=0.4)
-      
+       
 
 class Page2(Page):
    def __init__(self, *args, **kwargs):
@@ -257,10 +296,17 @@ class Page2(Page):
         except:
           pass
         def openfile():
-          print("some")
         # mshfile=filedialog.askopenfilename(title='Select file', filetypes=(('Msh files', '*.msh'), ('all files', '*.*')))
         # mshfilename.set(ntpath.basename(mshfile))
         # os.system("cp %s %s/%s/pre-processing"%(mshfile, folder, project_name))
+          timeStr = time.asctime()
+          msg="Time: "+timeStr
+          logging.info(msg)
+          filehandle.write(msg+"\n")
+          msg="%s is now present in pre-processing folder."%mshfilename.get()
+          logging.info(msg)
+          logging.info("\n")
+          filehandle.write(msg+"\n")
         openfile()
         bottom.bind('<Control-o>', openfile)
        button=tk.Button(bottom, text="Browse", font=helv36, command=selectmsh)
@@ -290,10 +336,17 @@ class Page2(Page):
         except:
           pass
         def openfile():
-          print("some")
         # flmlfile=filedialog.askopenfilename(title='Select file', filetypes=(('Flml files', '*.flml'), ('all files', '*.*')))
         # flmlfilename.set(ntpath.basename(flmlfile))
         # os.system("cp %s %s/%s"%(flmlfile, folder, project_name))
+          timeStr = time.asctime()
+          msg="Time: "+timeStr
+          logging.info(msg)
+          filehandle.write(msg+"\n")
+          msg="%s is now present in %s folder."%(flmlfilename.get(), project_name)
+          logging.info(msg)
+          logging.info("\n")
+          filehandle.write(msg+"\n")
         openfile()
         bottom.bind('<Control-o>', openfile)
        button=tk.Button(bottom, text="Browse", font=helv36, command=selectflml)
@@ -301,9 +354,25 @@ class Page2(Page):
        button.place(relwidth=0.15, relheight=0.15, rely=0.55, relx=0.8)
 
        def createvtu():
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="Fluidity is running..."
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
         # os.chdir(r"%s/%s"%(folder, project_name))
         # os.system("fluidity ./solver/%s"%flmlfilename.get())
         # os.system("find %s/%s/ -maxdepth 1 -type f -print0 | xargs -0 mv -t %s/%s/postprocessor"%(folder, project_name, folder, project_name))
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="All vtu files are now present in postprocessor folder."
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
         sv2.set("1")
         postfoldername.set("%s/%s/postprocessor"%(folder, project_name))
        def callback(sv):
@@ -314,7 +383,6 @@ class Page2(Page):
        buttonsolve.pack()
        buttonsolve.place(relwidth=0.20, relheight=0.15, rely=0.75, relx=0.4)
 
-       
 class Page3(Page):
    def __init__(self, *args, **kwargs):
        Page.__init__(self, *args, **kwargs)
@@ -391,6 +459,14 @@ class Page3(Page):
         # postfolder = tk.filedialog.askdirectory()
         # postfoldername.set(postfolder)
         # os.system(""find %s/ -maxdepth 1 -type f -print0 | xargs -0 cp -t %s/%s/postprocessor"%(postfolder, folder, project_name)")
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="Selected folder files are now in postprocessor folder."
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
        button=tk.Button(bottom, text="Browse", font=helv36, command=selectpost)
        button.pack()
        button.place(relwidth=0.15, relheight=0.15, rely=0.2, relx=0.8)  
@@ -399,15 +475,45 @@ class Page3(Page):
           buttonpost.config(state="normal")
        sv2.trace("w", lambda name, index, mode, sv=sv2: callback(sv2))
        def paraview():
-        print("some")
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="Running Paraview..."
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
         # os.system("find %s/files/ -maxdepth 1 -type f -print0 | xargs -0 cp -t %s/%s/postprocessor"%(curdir, folder, project_name))
         # os.chdir(r"%s/%s/postprocessor"%(folder, project_name))
         # os.system("python plot_data.py")
-        
+        timeStr = time.asctime()
+        msg="Time: "+timeStr
+        logging.info(msg)
+        filehandle.write(msg+"\n")
+        msg="Output files are created and are present in postprocessor folder."
+        logging.info(msg)
+        logging.info("\n")
+        filehandle.write(msg+"\n")
        buttonpost=tk.Button(bottom, text="Run paraview", font=helv36, command=paraview, state="disabled")
        buttonpost.pack()
        buttonpost.place(relwidth=0.20, relheight=0.15, rely=0.5, relx=0.4)
 
+def head(self):
+  st = ScrolledText.ScrolledText(self, state='normal')
+  st.configure(font='TkFixedFont', bg="white")
+  st.pack(expand="true", fill="both")
+
+  # Create textLogger
+  text_handler = TextHandler(st)
+  
+  # Logging configuration
+  logging.basicConfig(filename='test.log',
+      level=logging.INFO, 
+      format='%(asctime)s - %(levelname)s - %(message)s')        
+  
+  # Add the handler to logger
+  logger = logging.getLogger()        
+  logger.addHandler(text_handler)
 
 class MainView(tk.Frame):
     def __init__(self, *args, **kwargs):
@@ -420,7 +526,8 @@ class MainView(tk.Frame):
 
         self.mainwindow.pack()
         self.logwindow.place(relwidth=0.3, relheight=1, relx=0.7)
-        self.mainwindow.place(relwidth=0.7, relheight=1) 
+        self.mainwindow.place(relwidth=0.7, relheight=1)
+        head(self.logwindow)    
         header=tk.Frame(self.mainwindow)
         header.pack()
         header.place(relwidth=1, relheight=0.1)
@@ -467,11 +574,33 @@ class MainView(tk.Frame):
         header.grid_columnconfigure(3, weight=1, uniform="group1")
         p1.show()
 
+class TextHandler(logging.Handler):
+    # This class allows you to log to a Tkinter Text or ScrolledText widget
+    
+    def __init__(self, text):
+        # run the regular Handler __init__
+        logging.Handler.__init__(self)
+        # Store a reference to the Text it will log to
+        self.text = text
+
+    def emit(self, record):
+        msg = self.format(record)
+        def append():
+            self.text.configure(state='normal')
+            self.text.insert(tk.END, msg + '\n')
+            self.text.configure(state='disabled')
+            # Autoscroll to the bottom
+            self.text.yview(tk.END)
+        self.text.after(0, append)
+
  
 if __name__ == "__main__":
     folder, project_name, flag, flag2=project.build()
+    logging.getLogger().setLevel(logging.INFO)
     if flag==0:
       exit(0)
+    if flag2==0:
+      filehandle = open("%s/%s/run.log"%(folder, project_name), 'w')
     root = tk.Tk()
     helv36 = font.Font(family='Helvetica', size=15)
     helv26 = font.Font(family='Helvetica', size=12)
@@ -507,9 +636,28 @@ if __name__ == "__main__":
     sv2.set("0")
 
     main = MainView(root)    
+    if flag2==1:
+      try:
+        filehandle=open("%s/%s/run.log"%(folder, project_name), 'r')
+        for x in filehandle:
+          logging.info(x)
+        filehandle.close()
+      except:
+        pass
+      filehandle=open("%s/%s/run.log"%(folder, project_name), 'a')
+
+    timeStr = time.asctime()
+    msg="Time: "+timeStr
+    logging.info(msg)
+    filehandle.write(msg+"\n")
+    msg="The current working folder is %s. It has subdirectories of pre-processing, solver and postprocessor."%project_name
+    logging.info(msg)
+    logging.info("\n")
+    filehandle.write(msg+"\n")
     root.title("CFD Software")
     main.pack(side="top", fill="both", expand=True)
     root.geometry("1500x1500")
     root.minsize(1200, 500)
     root.mainloop()
+    filehandle.close()
     
